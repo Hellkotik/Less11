9e0bed87487d1e5d7e510406685d7609e5f1d476
Katerina <iglkotik@gmail.com>
2024-11-24 17:34:54 +0300

    Добавьте файлы проекта.

---
+++ Less11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа k и b");
+            double k=Convert.ToDouble(Console.ReadLine());
+            double b=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ваше уравнение иммет вид: {0}x+{1}=0", k, b);
+            Urav urav = new Urav(k, b);
+            Console.WriteLine(urav.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Less11/Urav.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less11
+{
+    struct Urav
+    {
+        double k;
+        double b;
+
+        public Urav(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root() 
+        {
+            double x = b / k;
+            if (k == 0 && b == 0)
+            {
+                return "Корней бесконечно много";
+            }
+            else if (k == 0 && b != 0)
+            { return "Корней нет"; }
+            else { return $"Корень х={x}"; }
+
+
+        }
+
+     
+
+
+    }
+}
